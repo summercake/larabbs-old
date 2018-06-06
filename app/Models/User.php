3845f092e30ae4c6b63b801be1b5e80d09b63d51
summercake@qq.com <summercake@qq.com>
2018-06-06 23:32:48 +0000

    old larabbs

---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class User extends Authenticatable
+{
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable
+        = [
+            'name', 'email', 'password', 'introduction', 'avatar',
+        ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden
+        = [
+            'password', 'remember_token',
+        ];
+
+    public function topics()
+    {
+        return $this -> hasMany(Topic::class);
+    }
+
+    public function isAuthorOf($model)
+    {
+        return $this -> id == $model -> user_id;
+    }
+
+    public function replies()
+    {
+        return $this -> hasMany(Reply::class);
+    }
+}
